c8dbbd670885cec4436243a92d92a535ab94b13a
zoribelle <zorib.contact@proton.me>
2026-09-21 20:15:42 -0600

    Fixed package installations. The ~/.config/appman setting file has been added to be managed by chezmoi. Related files has been adjusted as a result

--- run_after_01-archive-configs.sh
+++ run_after_01-archive-configs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Completed! Backing a tar of chezmoi settings..."; sleep 1s
-tar cf dotfiles.tar "${HOME}/.local/share/chezmoi/"*
+chezmoi archive --gzip --output="${HOME}"/dotfiles.tar.gz
